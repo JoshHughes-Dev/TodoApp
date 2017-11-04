eaea4cd093bea22e684a6c6035e9347d91118356
Josh <joshhughes.dev@gmail.com>
2017-11-04 22:54:56 +0000

    added enter (explode) animation for splash

--- app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.support.v7.app.AppCompatActivity
+import android.transition.Explode
+import android.view.Window
 import com.jhughes.todoapp.R
+
+
 
 class SplashActivity : AppCompatActivity() {
 
-    lateinit var handler : Handler
+    private lateinit var handler : Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+        window.enterTransition = Explode()
+
         setContentView(R.layout.activity_splash)
 
         handler = Handler(Looper.getMainLooper())
+
     }
 
     override fun onStart() {
