0941178472893ac7ba8ddac8da1c7a2a0c1aa4e0
Josh <joshhughes.dev@gmail.com>
2017-11-06 22:14:14 +0000

    yay added base observable work around until viewModel extends baseObservable possibly in future

--- app/src/main/java/com/jhughes/todoapp/ui/viewModel/BaseViewModel.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/viewModel/BaseViewModel.kt
@@ -3,10 +3,30 @@ package com.jhughes.todoapp.ui.viewModel
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.content.Context
+import android.databinding.Observable
+import android.databinding.PropertyChangeRegistry
 
-open class BaseViewModel(application : Application) : AndroidViewModel(application) {
+open class BaseViewModel(application : Application) : AndroidViewModel(application), Observable {
 
-    val context : Context
+    protected val context : Context
         get() = getApplication()
+
+    private val registry: PropertyChangeRegistry = PropertyChangeRegistry()
+
+    override fun addOnPropertyChangedCallback(onPropertyChangedCallback: Observable.OnPropertyChangedCallback) {
+        registry.add(onPropertyChangedCallback)
+    }
+
+    override fun removeOnPropertyChangedCallback(onPropertyChangedCallback: Observable.OnPropertyChangedCallback) {
+        registry.remove(onPropertyChangedCallback)
+    }
+
+    fun notifyChange() {
+        registry.notifyCallbacks(this, 0, null)
+    }
+
+    fun notifyPropertyChanged(fieldId: Int) {
+        registry.notifyCallbacks(this, fieldId, null)
+    }
 
 }
--- app/src/main/java/com/jhughes/todoapp/ui/viewModel/MainViewModel.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/viewModel/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.jhughes.todoapp.ui.viewModel
 
 import android.app.Application
+import android.databinding.Bindable
 import android.databinding.ObservableField
 import android.widget.Toast
+import com.jhughes.todoapp.BR
 import javax.inject.Inject
 
 
@@ -10,13 +12,21 @@ class MainViewModel @Inject internal constructor(
         application: Application) : BaseViewModel(application) {
 
     val testText = ObservableField("")
+    var hasclicked = false
 
     init {
-        testText.set("Hi my name is josh")
+        testText.set("Hi my name is Josh")
+    }
+
+    @Bindable
+    fun getTestTextTwo(): CharSequence {
+        return if(hasclicked) "yay its so cool" else "i made an app"
     }
 
     fun fabClick() {
         Toast.makeText(context, "fab clicked", Toast.LENGTH_LONG).show()
+        hasclicked = true;
         testText.set("ahhhh ya clicked it")
+        notifyPropertyChanged(BR.testTextTwo)
     }
 }
