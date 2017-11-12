ff68227867786f7b8c423eeee517b45442910a8c
Josh <joshhughes.dev@gmail.com>
2017-11-12 19:11:03 +0000

    tweaked naming

--- app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity
 import android.transition.Explode
 import android.util.Log
 import android.view.Window
-import com.jhughes.todoapp.R
 import com.jhughes.todoapp.TodoApplication
 import com.jhughes.todoapp.databinding.ActivitySplashBinding
 import com.jhughes.todoapp.injection.component.DaggerSplashActivityComponent
@@ -23,12 +22,34 @@ class SplashActivity : AppCompatActivity()  {
 
     private lateinit var component : SplashActivityComponent
     private lateinit var binding : ActivitySplashBinding
+    private lateinit var viewModel : SplashViewModel
 
     @Inject lateinit var viewModelFactory: SplashViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setUpInjection();
+
+        setUpWindowTransition()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(SplashViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        setContentView(binding.root)
+
+        viewModel.splashCompleteEvent.observe(this, Observer<Void> {
+            onOpenMain()
+        })
+
+        lifecycle.addObserver(binding.viewModel as LifecycleObserver)
+    }
+
+    private fun setUpInjection() {
         val todoApp = application as TodoApplication
 
         component = DaggerSplashActivityComponent.builder()
@@ -37,21 +58,11 @@ class SplashActivity : AppCompatActivity()  {
                 .build()
 
         component.inject(this)
+    }
 
+    private fun setUpWindowTransition() {
         window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
         window.enterTransition = Explode()
-
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        binding.viewModel = ViewModelProviders.of(this, viewModelFactory)
-                .get(SplashViewModel::class.java)
-
-        setContentView(R.layout.activity_splash)
-
-        binding.viewModel?.startActivityEvent!!.observe(this, Observer<Void> {
-            onOpenMain()
-        })
-
-        lifecycle.addObserver(binding.viewModel as LifecycleObserver)
     }
 
     private fun onOpenMain() {
--- app/src/main/java/com/jhughes/todoapp/ui/viewModel/SplashViewModel.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/viewModel/SplashViewModel.kt
@@ -13,19 +13,14 @@ class SplashViewModel @Inject internal constructor(
         application: Application,
         private val uiThread: Handler) : BaseViewModel(application), LifecycleObserver {
 
-    val startActivityEvent = SingleLiveEvent<Void>()
+    val splashCompleteEvent = SingleLiveEvent<Void>()
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startMain() {
         uiThread.postDelayed({
             Log.d("Navigate", "called navigator")
-            startActivityEvent.call()
+            splashCompleteEvent.call()
         }, 1500)
     }
 
-    //todo navigator idea
-    //navigator handle observable navigation stuff
-    //scoped to viewModel so dont hold reference to view
-    //have view subscribe to that.
-    //therefore delagate all actions to navigator class who is observed by view
 }
