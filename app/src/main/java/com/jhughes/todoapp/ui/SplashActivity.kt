1e699b924d19d87772a8c6d17ae9a7a5a1568a32
Josh <joshhughes.dev@gmail.com>
2017-11-19 14:09:42 +0000

    added idea for navigator class - not perfect though

--- app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/SplashActivity.kt
@@ -3,11 +3,13 @@ package com.jhughes.todoapp.ui
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.jhughes.todoapp.TodoApplication
+import com.jhughes.todoapp.data.Navigator
 import com.jhughes.todoapp.databinding.ActivitySplashBinding
 import com.jhughes.todoapp.injection.component.DaggerSplashActivityComponent
 import com.jhughes.todoapp.injection.component.SplashActivityComponent
@@ -18,20 +20,20 @@ import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
 
 
-class SplashActivity : AppCompatActivity()  {
+class SplashActivity : AppCompatActivity() {
 
-    private lateinit var component : SplashActivityComponent
-    private lateinit var binding : ActivitySplashBinding
-    private lateinit var viewModel : SplashViewModel
+    private lateinit var component: SplashActivityComponent
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var viewModel: SplashViewModel
 
     @Inject lateinit var viewModelFactory: SplashViewModelFactory
 
-    private var hasPerformedTransistion = false
+    private var hasPerformedTransition = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setUpInjection();
+        setUpInjection()
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
 
@@ -42,11 +44,23 @@ class SplashActivity : AppCompatActivity()  {
 
         setContentView(binding.root)
 
-        viewModel.splashCompleteEvent.observe(this, Observer<Void> {
-            onOpenMain()
-        })
+        viewModel.navigationEvent.observe(this, Observer<Navigator>({ navigator ->
+            handleNavigationEvent(navigator)
+        }))
 
         lifecycle.addObserver(binding.viewModel as LifecycleObserver)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (hasPerformedTransition) {
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(binding.viewModel as LifecycleObserver)
     }
 
     private fun setUpInjection() {
@@ -60,25 +74,44 @@ class SplashActivity : AppCompatActivity()  {
         component.inject(this)
     }
 
-    private fun onOpenMain() {
-        Log.d("Navigate", "openMainMenu")
+    private fun handleNavigationEvent(navigator: Navigator?) {
 
-        val intent = MainActivity.getStartIntent(this);
-        var options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, splash_icon, "splash")
+        if (navigator == null) {
+            return
+        }
 
-        hasPerformedTransistion = true
-        startActivity(intent, options.toBundle())
-    }
+        if (navigator.startActivity != null) {
+            val intent = Intent(this, navigator.startActivity)
+            if (navigator.bundle != null) {
+                intent.putExtras(navigator.bundle)
+            }
+            if (navigator.requestCode > -1) {
+                startActivityForResult(intent, navigator.requestCode)
+            } else {
+                startActivity(intent)
+            }
+        } else if (navigator.key != null) {
+            handleNavigationKeyEvent(navigator)
+        }
 
-    override fun onStop() {
-        super.onStop()
-        if(hasPerformedTransistion) {
-            finish()
+        if (navigator.isFinishActivity) {
+            //normally finish but here do something different
+            hasPerformedTransition = true
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        lifecycle.removeObserver(binding.viewModel as LifecycleObserver)
+    private fun handleNavigationKeyEvent(navigator: Navigator) {
+        if (navigator.key == SplashViewModel.KEY_OPEN_MAIN) {
+            onOpenMain()
+        }
+    }
+
+    private fun onOpenMain() {
+        Log.d("Navigate", "openMainMenu")
+
+        val intent = MainActivity.getStartIntent(this)
+        var options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, splash_icon, "splash")
+
+        startActivity(intent, options.toBundle())
     }
 }
--- app/src/main/java/com/jhughes/todoapp/ui/viewModel/SplashViewModel.kt
+++ app/src/main/java/com/jhughes/todoapp/ui/viewModel/SplashViewModel.kt
@@ -7,20 +7,38 @@ import android.arch.lifecycle.OnLifecycleEvent
 import android.os.Handler
 import android.util.Log
 import com.jhughes.todoapp.SingleLiveEvent
+import com.jhughes.todoapp.data.Navigator
 import javax.inject.Inject
 
 class SplashViewModel @Inject internal constructor(
         application: Application,
         private val uiThread: Handler) : BaseViewModel(application), LifecycleObserver {
 
-    val splashCompleteEvent = SingleLiveEvent<Void>()
+    companion object {
+        val KEY_OPEN_MAIN = "key_splash_open_main"
+    }
+
+    val navigationEvent = SingleLiveEvent<Navigator>()
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startMain() {
         uiThread.postDelayed({
             Log.d("Navigate", "called navigator")
-            splashCompleteEvent.call()
+            splashCompleteEvent()
         }, 1500)
+    }
+
+    private fun splashCompleteEvent() {
+
+//        navigationEvent.value = Navigator.newBuilder()
+//                .withActivity(MainActivity::class.java)
+//                .withFinish()
+//                .build()
+        navigationEvent.value = Navigator.newBuilder()
+                .withKey(KEY_OPEN_MAIN)
+                .build()
+
+        navigationEvent.call()
     }
 
 }
